44fa28b48c39b1d3c7cce51b61843b5b1102a6da
Fareed Javed <fareedjaved@Fareeds-MacBook-Pro.local>
2016-05-11 20:28:08 -0400

    Update arrests.rb

--- arrests.rb
+++ arrests.rb
@@ -57,18 +57,18 @@ percent2 = (w.to_f / unknown.to_f) * 100
 printf("%2d",percent2)
 puts " percent of those who were arrested for an unknown offense"
 
-print "this means that 1 out of 5 people who are arrested in Baltimore are done so for an unknown reason. Additionally, 
+puts 
+print "This means that around 1 out of 5 people who are arrested in Baltimore are done so for an unknown reason. Additionally, 
 	every time that 1 person is arrested, theres a 4/5 chance that the person is African American"
 
 puts 
-puts "Furthermore, it is important to analyze what the charges were for those who were arrested for an unknown offense..."
-puts "Of the African Americans who were arrested, #{b_loit} were charged with either Loitering or an Unknown Offense"
-printf("That means that %2d percent of African Americans who were arrested for an Unknown Offense were charged with Loitering, aka, standing around. 
-	Or for no reason at all", (b_loit.to_f/b.to_f)*100)
+puts "Lets look at the charges were for those who were arrested for an unknown offense..."
+puts "Of the African Americans who were arrested, #{b_loit} were charged with Loitering or an Unknown Offense"
+printf("That means that %2d percent of African Americans who were arrested for an Unknown Offense were charged with Loitering or 
+	for no reason at all", (b_loit.to_f/b.to_f)*100)
 
 puts 
 puts "The number of white people who were arrested for an Unknown Offense and were charged with Loitering, or with an Unknown Offense 
 	were #{w_loit}"
-printf("That means that %2d percent of White Americans who were arrested for an Unknown Offense were charged with Loitering, 
-	or for no reason at all", (w_loit.to_f/w.to_f)*100)
+puts "That means that African Americans were 5 times as likely to be arrested for standing around than a white person"
 
