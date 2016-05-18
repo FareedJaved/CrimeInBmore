bde5ed3461a7aa37eb82b715e8b5bf2e9b4301db
Fareed Javed <fareedjaved@gmail.com>
2016-05-17 21:30:53 -0400

    Changing all graphs to doughnut charts

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ $(function() {
 	// 2013 data 
 	var data13 = [ 
 		{value: 11976, color: "Purple"}, 
-		{value: 2349, color: "#cc6600"}
-	]
+		{value: 2349, color: "#ffb31a"}
+	];
 
 	// How I want the chart to behave while being loaded on page
 	var doughnutOptions = {
-  		segmentStrokeColor: "white",
+  		segmentStrokeColor: "black",
   		segmentStrokeWidth: 3,
   		percentageInnerCutout: 50,
   		animation : true, 
@@ -27,47 +27,22 @@ $(function() {
 // =================================================================
 
 	// 2014 data
-	var data14 = {
-		labels: ["White","African Americans"], 
-		datasets: [
-			{
-				label:"2013",
-	            backgroundColor: "plum",
-	            fillColor: "RoyalBlue",
-	            borderColor: "rgba(153,0,76,1)",
-	            borderWidth: 1,
-	            hoverBackgroundColor: "rgba(192,192,192,0.3)",
-	            hoverBorderColor: "rgba(153,0,76,1)",
-	            data: [4950, 24107], 
-						
-			}
-		]
-	};
-
-	// 2015 data
-	var data15 = {
-		labels: ["White","African Americans"], 
-		datasets: [
-			{
-				label:"2015",
-	            backgroundColor: "plum",
-	            fillColor: "ForestGreen",
-	            borderColor: "rgba(153,0,76,1)",
-	            borderWidth: 1,
-	            hoverBackgroundColor: "rgba(192,192,192,0.3)",
-	            hoverBorderColor: "rgba(153,0,76,1)",
-	            data: [3671, 18501], 		
-			}
-		]
-	};
-
-	var option = {};
-
-	/* All three chart instantiations */
+	var data14 = [
+		{value: 11976, color: "Purple"}, 
+		{value: 2349, color: "#ffb31a"}
+	];
 
 	var ctx2 = document.getElementById("2014BarChart").getContext("2d");
-	var barChart14 = new Chart(ctx2).Bar(data14,option);
+	var barChart14 = new Chart(ctx2).Doughnut(data14,doughnutOptions);
+
+// ==================================================================
+
+	// 2015 data
+	var data15 = [
+		{value: 18501, color: "Purple"}, 
+		{value: 3671, color: "#ffb31a"}
+	];
 
 	var ctx3 = document.getElementById("2015BarChart").getContext("2d");
-	var barChart15 = new Chart(ctx3).Bar(data15,option);
+	var barChart15 = new Chart(ctx3).Doughnut(data15,doughnutOptions);
 })
